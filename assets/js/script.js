e65bb1c788a82c2d53e4be26868ec341cd71dcbd
Douglas Leal <douglas.leal@live.com>
2023-04-26 19:14:42 -0300

    adicionando script para link active

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+let scrollTop = document.documentElement.scrollTop;
+let secoes = document.querySelectorAll(".secao");
+let navLinks = document.querySelectorAll(".nav-link");
+
+let ultimaSecao = null;
+
+onscroll = () => {
+    const scrollTop = document.documentElement.scrollTop + 85;
+
+    secoes.forEach(secao => {
+        let secaoTop = secao.getBoundingClientRect().top + window.pageYOffset;
+        let secaoBottom = secao.getBoundingClientRect().bottom + window.pageYOffset;
+
+        if(scrollTop >= secaoTop && scrollTop <= secaoBottom){
+            if(ultimaSecao !== secao){
+                ultimaSecao = secao;
+
+                navLinks.forEach(link => {
+                    if(link.getAttribute("href").includes(secao.id)){
+                        link.classList.add("active");
+                    }else{
+                        link.classList.remove("active");
+                    }
+                });                
+            }           
+        }
+
+        if(scrollTop < 300){
+            ultimaSecao = null;
+            limparActive();
+            navLinks[0].classList.add("active");
+        }
+    });
+}
+
+function limparActive(){
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+    });
+}
