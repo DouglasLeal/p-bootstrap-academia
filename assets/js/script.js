01f5a345bff4d7fb2e649d0104751b8311721b1b
Douglas Leal <douglas.leal@live.com>
2023-04-26 20:05:51 -0300

    ajust info-topo

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,36 +41,6 @@ function limparActive() {
 
 navLinks.forEach(link => {
     link.onclick = (event) => {
-        event.preventDefault();
-
         document.querySelector(".navbar-collapse").classList.remove("show");
-
-        smoothScroll(link);
     }
 })
-
-function smoothScroll(target) {
-    let targetElement = document.querySelector(`${target.href.split("/")[3]}`);
-    let targetPosition = targetElement.offsetTop;
-    let startPosition = window.pageYOffset;
-    let distance = targetPosition - startPosition;
-    let duration = 1000;
-    let startTime = null;
-
-    function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        let timeElapsed = currentTime - startTime;
-        let run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
-        t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-}
